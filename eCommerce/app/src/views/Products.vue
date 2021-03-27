85afeff4b9787fe92b860c20393566fc8db9a789
Iman.ezziyadi <iman.ezziyadi@utb.ecutbildning.se>
2021-03-27 21:37:54 +0100

    updatr

---
+++ eCommerce/app/src/views/Products.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container mt-5">
+    <h1> Products</h1>
+    <product-list />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import ProductList from '../components/products/ProductList'
+export default {
+  components: {
+    ProductList
+  },
+  methods: {
+    ...mapActions(['getProducts'])
+  },
+  created() {
+    this.getProducts()
+  }
+}
+</script>
+
+<style>
+
+</style>
